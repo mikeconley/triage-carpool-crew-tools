4c3d1006e75b30894e83002b560bb3082b0d4253
Mike Conley <mike@conley.io>
2024-03-04 11:40:48 -0500

    Update triager chooser to just choose the person with fewest bugs assigned per selection.

--- triage.py
+++ triage.py
@@ -6,8 +6,6 @@ import logging
 import requests
 import sys
 
-from random import choices
-
 """
 A script to find the bugs for triage, and to distribute them evenly
 to the team to perform triage asynchronously.
@@ -26,7 +24,7 @@ Thanks,
 -Your friendly triage list generator
 """
 
-LIST_URL = "https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,status,creator&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&classification=Client%20Software&classification=Developer%20Infrastructure&classification=Components&classification=Server%20Software&classification=Other&f1=OP&f10=component&f11=component&f12=component&f13=component&f14=product&f15=bug_type&f16=OP&f17=priority&f18=bug_severity&f19=CP&f2=triage_owner&f3=triage_owner&f4=triage_owner&f5=triage_owner&f6=triage_owner&f7=triage_owner&f8=CP&f9=creation_ts&j1=OR&j16=OR&keywords=meta&keywords_type=nowords&o10=notequals&o11=notequals&o12=notequals&o13=notequals&o14=notequals&o15=notequals&o17=equals&o18=equals&o2=equals&o3=equals&o4=equals&o5=equals&o6=equals&o7=equals&o9=greaterthan&resolution=---&v10=File%20Handling%20&v11=%20mozscreenshots&v12=Picture-in-Picture%20&v13=Shopping&v14=Flowstate&v15=enhancement&v17=--&v18=--&v2=mhowell%40mozilla.com&v3=mconley%40mozilla.com&v4=gijskruitbosch%2Bbugs%40gmail.com&v5=jhirsch%40mozilla.com&v6=cmeador%40mozilla.com&v7=achurchwell%40mozilla.com&v9=2022-01-01"
+LIST_URL = "https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,status,creator&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&classification=Client%20Software&classification=Developer%20Infrastructure&classification=Components&classification=Server%20Software&classification=Other&f1=OP&f10=component&f11=component&f12=component&f13=component&f14=component&f15=product&f16=bug_type&f17=OP&f18=priority&f19=bug_severity&f2=triage_owner&f20=CP&f3=triage_owner&f4=triage_owner&f5=triage_owner&f6=triage_owner&f7=triage_owner&f8=CP&f9=creation_ts&j1=OR&j17=OR&keywords=meta&keywords_type=nowords&o10=notequals&o11=notequals&o12=notequals&o13=notequals&o14=notequals&o15=notequals&o16=notequals&o18=equals&o19=equals&o2=equals&o3=equals&o4=equals&o5=equals&o6=equals&o7=equals&o9=greaterthan&resolution=---&v10=File%20Handling%20&v11=%20mozscreenshots&v12=Picture-in-Picture%20&v13=Shopping&v14=New%20Tab%20Page&v15=Flowstate&v16=enhancement&v18=--&v19=--&v2=mhowell%40mozilla.com&v3=mconley%40mozilla.com&v4=gijskruitbosch%2Bbugs%40gmail.com&v5=jhirsch%40mozilla.com&v6=cmeador%40mozilla.com&v7=achurchwell%40mozilla.com&v9=2022-01-01"
 
 BUGZILLA_URL = "https://bugzilla.mozilla.org/buglist.cgi?quicksearch=%s"
 
@@ -66,13 +64,9 @@ def main(options):
         TEAM[victim_key]['bugs'] = []
     for index, bug in enumerate(data['bugs']):
         possible_triagers = list(filter(lambda t: bug['creator'] != TEAM[t]['email'], active_team_keys))
-        # Use weighting to try to balance the number of bugs per person,
-        # based on the number of bugs assigned per person divided by the
-        # number of bugs assigned so far (plus 1, to avoid division by 0 for
-        # the first bug).
-        weighting = list(map(lambda t: 1 - (len(TEAM[t]['bugs']) / (index+1)), possible_triagers))
-        random_victim = choices(possible_triagers, weights=weighting)[0]
-        TEAM[random_victim]['bugs'].append(bug)
+        # Get the one with the fewest bugs to triage
+        chosen_victim = sorted(possible_triagers, key=lambda t: len(TEAM[t]['bugs']))[0]
+        TEAM[chosen_victim]['bugs'].append(bug)
 
     logging.info("Distribution completed")
     bug_lists = ""
